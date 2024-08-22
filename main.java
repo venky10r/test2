6e3510adb7267c4d6ca56580c8a786b362b41066
venky10r <sonuvenky39@gmail.com>
2024-08-22 19:26:19 +0530

    my app

---
+++ main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args){
+        for (int i =0; i<5; i++){
+            System.out.println("#");
+            
+            
+            
+        }
+    }
+}
